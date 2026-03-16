ab13d7a5b5cf8ff4ffb25a75bfe24c35c9f0fedd
burnseysco <gerryburns39@gmail.com>
2026-03-16 14:27:09 +0000

    Laravel Learn course completed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\Login;
+use App\Http\Controllers\Auth\Logout;
 use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\Register;
@@ -20,5 +22,9 @@ Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
 
 Route::view('/register', 'auth.register')->middleware('guest')->name('register');
 Route::post('/register', Register::class)->middleware('guest');
+
+Route::view('login', 'auth.login')->middleware('guest')->name('login'); 
+Route::post('/login', Login::class)->middleware('guest');
+Route::post('/logout', Logout::class)->middleware('auth')->name('logout');
 
  
